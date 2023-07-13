3feb58500363e1e972ada30d0c172a7f124564a0
USER-NOVASOFT <USER-NOVASOFT@DESKTOP-LVAEFSD>
2023-07-13 11:16:00 +0000

    Ajoutez des fichiers projet.

---
+++ Exercice1V2/Controllers/NewsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+public class NewsController : Controller
+{
+    public async Task<ActionResult> Index()
+    {
+        string apiKey = "5d7d85b601f14a808539d4ccb73a2976";
+        string apiUrl = $"https://newsapi.org/v2/top-headlines?country=us&apiKey={apiKey}";
+
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Traitez les données de réponse et transmettez-les à la vue
+            return View(responseBody);
+        }
+    }
+}
---
+++ Exercice1V2/Models/RouteConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Web.Mvc;
+using System.Web.Routing;
+
+public class RouteConfig
+{
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        routes.MapRoute(
+            name: "Default",
+            url: "{Controller}/{action}/{id}",
+            defaults: new { controller = "News", action = "Index", id = UrlParameter.Optional }
+        );
+    }
+}
